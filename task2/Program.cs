f8a2562da8e7b768f169fe44d1d98197bc370323
doomkinn-geek <doomkinn@yandex.ru>
2021-04-08 17:03:27 +0300

    task2 almost done

---
+++ task2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] handBook = new string[5, 2];
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Введите имя: ");
+                handBook[i, 0] = Console.ReadLine();
+                Console.Write("Введите номер телефона/email: ");
+                handBook[i, 1] = Console.ReadLine();
+            }
+            Console.WriteLine("Получившийся телефонный справочник:");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"{handBook[i, 0]} \t\t\t {handBook[i, 1]}");
+            }
+        }
+    }
+}
