079cca23ac76dd018686591d2b2b10c2142869cc
doomkinn-geek <doomkinn@yandex.ru>
2021-04-08 16:53:02 +0300

    Добавьте файлы проекта.

---
+++ task1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int aSize = 10;
+            int[,] arr = new int[aSize, aSize];
+            var rand = new Random();
+            for (int i = 0; i < aSize; i++)
+            {
+                for (int j = 0; j < aSize; j++)
+                {
+                    arr[i, j] = rand.Next(50);
+                    Console.Write($"{arr[i, j]}\t");                    
+                }
+                Console.WriteLine("\n");
+            }
+            Console.WriteLine("Элементы массива из диагонали:");
+            for (int j = 0; j < aSize; j++)
+            {
+                Console.Write($"{arr[j, j]}\t");
+            }
+        }
+    }
+}
