9481ef2a00104708128ec1f5b0fa6004f6a40d00
doomkinn-geek <doomkinn@yandex.ru>
2021-04-09 10:54:00 +0300

    task 3 done

---
+++ task3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            Console.Write("Введите строку: ");
+            str = Console.ReadLine();
+            for(int i = str.Length-1; i > -1; i--)
+            {
+                Console.Write($"{str[i]} ");
+            }
+        }
+    }
+}
