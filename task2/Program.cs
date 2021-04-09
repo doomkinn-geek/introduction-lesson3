a0a5909c725a858f68fdb56a7bf708dce3e52eba
doomkinn-geek <doomkinn@yandex.ru>
2021-04-09 10:45:31 +0300

    task 2 fixed

--- task2/Program.cs
+++ task2/Program.cs
@@ -14,10 +14,20 @@ namespace task2
                 Console.Write("Введите номер телефона/email: ");
                 handBook[i, 1] = Console.ReadLine();
             }
+            /*handBook[0, 0] = "doomkinn";
+            handBook[0, 1] = "d@ya";
+            handBook[1, 0] = "roma";
+            handBook[1, 1] = "roman@gmail.com";
+            handBook[2, 0] = "alexander";
+            handBook[2, 1] = "a@v";
+            handBook[3, 0] = "vk";
+            handBook[3, 1] = "vk@bk";
+            handBook[4, 0] = "faterial";
+            handBook[4, 1] = "f@ya.ru";*/
             Console.WriteLine("Получившийся телефонный справочник:");
             for (int i = 0; i < 5; i++)
             {
-                Console.WriteLine($"{handBook[i, 0]} \t\t\t {handBook[i, 1]}");
+                Console.WriteLine("{0, -25} \t- {1, -15}", handBook[i, 0], handBook[i, 1]);
             }
         }
     }
